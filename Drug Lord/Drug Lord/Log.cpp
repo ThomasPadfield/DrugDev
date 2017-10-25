b62fbc1b7a81b1ca675e3e4b538954464396e337
sshuy <sshuy@protonmail.ch>
2017-10-25 05:54:04 +0100

    update

--- Drug Lord/Drug Lord/Log.cpp
+++ Drug Lord/Drug Lord/Log.cpp
@@ -1,12 +1,11 @@
 #include <iostream>
 
-<<<<<<< HEAD
 void Log(const char* letter)
 {
 	std::cout << letter << std::endl;
-=======
-void Log(const char* yugiohy)
+}
+
+void Logs(const char* yugiohy)
 {
 	std::cout << yugiohy << std::endl;
->>>>>>> 4d04ded46052c2a8d0a6b32a3251e98b3a2524bf
 }
